89b5a7ac28a200afc06952f3145fa7f35896f507
Marwam Malta <marwamprog.js@gmail.com>
2023-12-21 19:45:39 -0300

    feat: Secao3 - Type array

---
+++ projeto_app/output/array.js
@@ -0,0 +1,7 @@
+"use strict";
+const filmesCategorias = ["Comédia", "Drama", "Romance", "Aventura"];
+const filmesAnos = [];
+for (let ano = 2000; ano <= new Date().getFullYear(); ano++) {
+    filmesAnos.push(ano);
+}
+console.log(filmesAnos);
