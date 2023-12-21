e09e5eb4e3a6b011ec9d8a56cf2214faef74f66d
Marwam Malta <marwamprog.js@gmail.com>
2023-12-21 19:29:04 -0300

    feat: Secao3 - Type boolean

---
+++ projeto_app/output/boolean.js
@@ -0,0 +1,8 @@
+"use strict";
+let admin = true;
+admin = Boolean(undefined);
+console.log(admin);
+let senha = "123456789";
+let senhaSegura;
+senhaSegura = (senha.length > 8);
+console.log(senhaSegura);
