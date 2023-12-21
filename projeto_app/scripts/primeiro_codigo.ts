215a8699d1eace8ce73658f2b5c5371e6e5f3a4d
Marwam Malta <marwamprog.js@gmail.com>
2023-12-21 18:34:36 -0300

    Secao1 - Configurando Ambiente e primeiros passos

---
+++ projeto_app/scripts/primeiro_codigo.ts
@@ -0,0 +1,21 @@
+console.log("Hello World");
+
+console.log("-----------------------");
+
+const nome = "João";
+
+console.log("Olá " + nome + ". Seja bem vindo!!!");
+
+console.log("-----------------------");
+
+class Produto {
+
+    nome: string;
+    valor: number;
+
+    constructor(produtoNome: string, produtoValor: number) {
+        this.nome = produtoNome;
+        this.valor = produtoValor;
+    }
+    
+}
