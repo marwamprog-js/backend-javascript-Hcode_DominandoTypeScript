f4eac59814f2b840763472730e11229adf61db3a
Marwam Malta <marwamprog.js@gmail.com>
2023-12-22 18:24:41 -0300

    feat: Secao4 - Funcoes

---
+++ projeto_app/output/funcoes.js
@@ -0,0 +1,11 @@
+"use strict";
+function mostrarMensagem(texto) {
+    console.log(texto);
+    return true;
+}
+console.log(mostrarMensagem("Hcode"));
+console.log(mostrarMensagem(100));
+console.log(mostrarMensagem({ url: "https://hcode.com.br" }));
+const show = (codigo) => {
+    return new Date();
+};
