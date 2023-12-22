019198bc42f9bc494934d65432078d150afa670a
Marwam Malta <marwamprog.js@gmail.com>
2023-12-22 15:04:09 -0300

    feat: Secao3 - Unknown

---
+++ projeto_app/output/unknown.js
@@ -0,0 +1,11 @@
+"use strict";
+let resultado;
+resultado = {
+    sucesso: true
+};
+resultado = "Deu tudo certo. OK!";
+resultado = 200;
+console.log("==============");
+const informacao = 150;
+const informacaoDesconhecida = 250;
+const primeiroNumero = informacao;
