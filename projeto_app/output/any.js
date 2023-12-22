530143489aa69073071d92a639679d0fe1a4f9a5
Marwam Malta <marwamprog.js@gmail.com>
2023-12-22 14:59:01 -0300

    feat: Secao 3 - Any

---
+++ projeto_app/output/any.js
@@ -0,0 +1,12 @@
+"use strict";
+let valor;
+valor = true;
+valor = 1000;
+valor = "Hcode";
+console.log(typeof valor);
+let despesaFixa;
+let despesaVariavel;
+despesaFixa = "150";
+despesaVariavel = 320;
+console.log(despesaFixa + despesaVariavel);
+let itens = [];
