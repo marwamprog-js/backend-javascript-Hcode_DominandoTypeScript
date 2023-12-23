4bf2a2865807bf6eb29548beb7893500d50e527b
Marwam Malta <marwamprog.js@gmail.com>
2023-12-23 13:27:33 -0300

    feat: Secao6 - Getters and Setters

---
+++ projeto_app/output/classe_getters_setters.js
@@ -0,0 +1,25 @@
+"use strict";
+class Permissao {
+    constructor(_nome, _nivel) {
+        this._nome = _nome;
+        this._nivel = _nivel;
+    }
+    get nome() {
+        return this._nome.toUpperCase();
+    }
+    set nome(novoNome) {
+        if (novoNome.length < 5) {
+            throw new Error("O nome da permissão deve ter no mínimo 5 letras");
+        }
+        this._nome = novoNome;
+    }
+}
+const permissao = new Permissao("Administrador", 1);
+console.log(permissao.nome);
+try {
+    permissao.nome = "adm";
+}
+catch (e) {
+    console.log(e.message);
+}
+console.log(permissao.nome);
---
+++ projeto_app/scripts/classe_getters_setters.ts
@@ -0,0 +1,38 @@
+class Permissao {
+
+    constructor(
+        private _nome: string,
+        private _nivel: number
+    ) {}
+
+    get nome() {
+        return this._nome.toUpperCase();
+    }
+
+    set nome(novoNome: string) {
+
+        if (novoNome.length < 5) {
+            throw new Error("O nome da permissão deve ter no mínimo 5 letras");
+        }
+        
+        this._nome = novoNome;
+
+    }
+
+}
+
+const permissao = new Permissao("Administrador", 1);
+
+console.log(permissao.nome);
+
+try {
+
+    permissao.nome = "adm";
+
+} catch (e: any) {
+
+    console.log(e.message);
+
+}
+
+console.log(permissao.nome);
