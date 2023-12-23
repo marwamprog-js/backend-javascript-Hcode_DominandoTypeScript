380f9489aabad84c2f51785c0b5b61e8c06d028a
Marwam Malta <marwamprog.js@gmail.com>
2023-12-23 13:12:09 -0300

    feat: Secao6 - Contrutor

---
+++ projeto_app/output/classe_construtor.js
@@ -0,0 +1,10 @@
+"use strict";
+class Pedido {
+    constructor(produto, valorTotal, previsaoEntrega) {
+        this.produto = produto;
+        this.valorTotal = valorTotal;
+        this.previsaoEntrega = previsaoEntrega;
+    }
+}
+const meuPedido = new Pedido("TV 64 polegadas", 2000, new Date("2021-05-01"));
+console.log(meuPedido);
