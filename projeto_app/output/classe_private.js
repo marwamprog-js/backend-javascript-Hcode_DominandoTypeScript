0f4ef51b85ce436dcb909bc35252e42efbebbd3f
Marwam Malta <marwamprog.js@gmail.com>
2023-12-23 12:29:30 -0300

    feat: Secao6 - private

---
+++ projeto_app/output/classe_private.js
@@ -0,0 +1,29 @@
+"use strict";
+class Banco {
+    constructor() {
+        this.cofreQtd = 10000;
+    }
+    debitarCofre(quantidade) {
+        if (this.cofreQtd >= quantidade) {
+            this.cofreQtd -= quantidade;
+            return this.cofreQtd;
+        }
+        else {
+            return "O cofre não possui a quantidade requisitada";
+        }
+    }
+    sacarDoCaixa(quantidade) {
+        return this.debitarCofre(quantidade);
+    }
+    sacarDoCaixaEletronico(quantidade) {
+        return this.debitarCofre(quantidade);
+    }
+}
+class Banco24Horas extends Banco {
+    sacar(qtd) {
+        return this.sacarDoCaixa(qtd);
+    }
+}
+const nubank = new Banco();
+const banco24horas = new Banco24Horas();
+banco24horas.sacar(2500);
