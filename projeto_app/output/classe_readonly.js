9dfae4a5759a1d795f554b00ac8d8a45875e4ae0
Marwam Malta <marwamprog.js@gmail.com>
2023-12-23 13:03:50 -0300

    feat: Secao6 - Private x Readonly

---
+++ projeto_app/output/classe_readonly.js
@@ -0,0 +1,10 @@
+"use strict";
+class Usuario {
+    constructor() {
+        this.id = "123123sdad232";
+        this.nome = "Joao";
+        this.senha = "123456";
+        this.dataCadastro = new Date("2021-01-01");
+    }
+}
+const usuario = new Usuario();
